0f9f3ac8cc6474e5fd88b8072de0a92a5cbbced7
unknown <dimitriosfl@gmail.com>
2025-11-27 02:42:54 +0200

    fix images path

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects()
                             <p className="text-gray-400 mb-4">A simple App to search and quickly find basic info for crypto coins. </p>
                             <div className="flex justify-between items-center">
                                 <a href="https://dimitriosfl.github.io/crypto-app/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project</a>
-                                <img src="/crypto-app.jpg" alt="Project preview" className="w-80 h-45 rounded-lg ml-2" />
+                                <img src="crypto-app.jpg" alt="Project preview" className="w-80 h-45 rounded-lg ml-2" />
                             </div>
                         </div>
                         <div className="p-6 rounded-xl border border-white/10 hovver:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,30,246,0.1)] transition-all">
@@ -22,7 +22,7 @@ export default function Projects()
                             <p className="text-gray-400 mb-4">Fully functioning Wordle-clone game with over 2000 words to guess and no limit on rounds to play!</p>
                             <div className="flex justify-between items-center">
                                 <a href="https://dimitriosfl.github.io/wordle-clone/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project</a>
-                                <img src="/wordle-clone.jpg" alt="Project preview" className="w-80 h-40 rounded-lg ml-2" />
+                                <img src="wordle-clone.jpg" alt="Project preview" className="w-80 h-40 rounded-lg ml-2" />
                             </div>
                         </div>
                         <div className="p-6 rounded-xl border border-white/10 hovver:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,30,246,0.1)] transition-all">
@@ -30,7 +30,7 @@ export default function Projects()
                             <p className="text-gray-400 mb-4">Search for country or town and see the current weather conditions.</p>
                             <div className="flex justify-between items-center">
                                 <a href="https://dimitriosfl.github.io/weather-app/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project</a>
-                                <img src="/weather-app.jpg" alt="Project preview" className="w-50 h-70 rounded-lg ml-2" />
+                                <img src="weather-app.jpg" alt="Project preview" className="w-50 h-70 rounded-lg ml-2" />
                             </div>
                         </div>
                         <div className="p-6 rounded-xl border border-white/10 hovver:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,30,246,0.1)] transition-all">
@@ -38,7 +38,7 @@ export default function Projects()
                             <p className="text-gray-400 mb-4">A simple Todo App with local storage.</p>
                             <div className="flex justify-between items-center">
                                 <a href="https://dimitriosfl.github.io/todo-list/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4" >View Project</a>
-                                <img src="/todo-list.jpg" alt="Project preview" className="w-80 h-50 rounded-lg ml-2" />
+                                <img src="todo-list.jpg" alt="Project preview" className="w-80 h-50 rounded-lg ml-2" />
                             </div>
                         </div>
                     </div>
